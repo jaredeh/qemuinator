d773f44a289693c5469eccccd1b5fec76269c219
jared <Jared Hulbert>
2014-04-29 23:17:18 -0700

    jenkins deps

--- config/wheezy_jenkins_deps.sh
+++ config/wheezy_jenkins_deps.sh
@@ -1,8 +1,6 @@
-#export http_proxy=http://10.0.2.2:8888
-#apt-get update
-#apt-get -y install sudo
-#cat /etc/sudoers | sed 's/root\tALL=(ALL:ALL) ALL/root\tALL = NOPASSWD: ALL\nuser\tALL = NOPASSWD: ALL/' > /etc/sudoers.new
-#mv /etc/sudoers.new /etc/sudoers
-#apt-get -y install default-jre
-echo "freedom!"
-uname -a
+export http_proxy=http://10.0.2.2:8888
+apt-get update
+apt-get -y install sudo
+cat /etc/sudoers | sed 's/root\tALL=(ALL:ALL) ALL/root\tALL = NOPASSWD: ALL\nuser\tALL = NOPASSWD: ALL/' > /etc/sudoers.new
+mv /etc/sudoers.new /etc/sudoers
+apt-get -y install default-jre
